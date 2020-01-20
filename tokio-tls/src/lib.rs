aab67b01413b104bcc2a1ee9d0b1d86488d75ec1
Ben Sully <ben.sully@egress.com>
2020-01-20 09:18:09 +0000

    Add method to expose SSL object of TlsStream on Linux

--- tokio-tls/src/lib.rs
+++ tokio-tls/src/lib.rs
@@ -177,6 +177,12 @@ impl<S> TlsStream<S> {
     {
         &mut self.0.get_mut().inner
     }
+
+    #[cfg(all(not(target_os = "macos"), not(windows), not(target_os = "ios")))]
+    /// Return a reference to the SSL object used for the stream.
+    pub fn ssl(&self) -> &native_tls::openssl::ssl::SslRef {
+        self.0.ssl()
+    }
 }
 
 impl<S> AsyncRead for TlsStream<S>
